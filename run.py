99f1a33ca84c9f047a3b46a6746d11cf6b5da3cf
Jaccojwang <danolago@gmail.com>
2019-04-07 10:27:28 +0300

    Create main function and add user actions
    Add instructions for user navigation and functions for user to create account, save account and delete account

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from user import User
 from cred import Cred
 
-def create_user(fname, lname, phone, email):
+def create_user(fname, lname, usrname, pssword):
     '''
     Function to create new user
     '''
@@ -15,17 +15,17 @@ def save_user(user):
     '''
     user.save_user()
 
-def delete_user():
+def delete_user(user):
     '''
     Function to delete user
     '''
     user.delete_user()
 
-def find_user():
+def find_user(username):
     '''
     Function that finds a user by number and returns the user
     '''
-    return User.find_by_number(number)
+    return User.find_by_username(username)
 
 def check_existing_users(number):
     '''
@@ -56,7 +56,7 @@ def find_cred():
     '''
     Function that finds a cred by number and returns the cred
     '''
-    return Cred.find_by_number(number)
+    return Cred.find_by_account(account)
 
 def check_existing_creds(number):
     '''
@@ -69,3 +69,70 @@ def display_creds():
     Function that returns all saved creds
     '''
     return Cred.display_creds()
+
+
+
+def main():
+    print("Hello, welcome to Password Locker.")
+    print("Use the following short codes for navigation: ca - create a new account, li - log in to your account, da - delete your account, exa - exit application")
+
+    while True:
+        short_code = input()
+        if short_code == 'ca':
+            print('Please fill in the following details to create a new account')
+            print("New User")
+            print("-"*10)
+
+            print ("First name ....")
+            f_name = input()
+
+            print("Last name ...")
+            l_name = input()
+
+            print("Username...")
+            username = input()
+
+            print("Ensure your password is secure by using uncommon passwords")
+            print("Password ...")
+            password = input()
+
+            print('\n')
+            save_user(create_user(f_name, l_name, username, password))
+            print('\n')
+
+            print('\n')
+            print(f"New User {f_name} {l_name} created")
+            print('\n')
+
+
+        elif short_code == 'li':
+            print("Welcome to log in")
+            # print("Enter your username: ")
+            # username = input()
+            # print("Enter your password")
+            # password = input()
+            #
+            # if (username == password ) :
+            #     print(f"Welcome name person")
+            # else("Sorry, wrong password")
+
+        elif short_code == 'da':
+            print('Enter the username of the account you would like to delete')
+            delete_name = input()
+            if check_existing_users(delete_name):
+                search_user = find_user(delete_name)
+                delete_user(search_user)
+                print(f"{search_user.first_name} {search_user.last_name} has been deleted")
+            else:
+                print("Account does not exist")
+
+        elif short_code == 'exa':
+            print('Bye')
+            break
+        else:
+            print("Please use one of the shortcodes provided")
+
+
+if __name__ == '__main__':
+
+    main()
